7ea273a203060b3cd94975f392cb3e999c830269
soo <21700528@haneong.ac.kr>
2022-09-02 14:33:25 +0900

    First Commit

---
+++ WordMasterProject/src/com/mycom/word/Main.java
@@ -0,0 +1,11 @@
+package com.mycom.word;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 안녕");
+
+	}
+
+}
