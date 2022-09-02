44e1b56772fedf558ce0f5d6a69fbad533f14db1
soo <21700528@haneong.ac.kr>
2022-09-02 16:28:48 +0900

    Third Commit : add selectMenu

--- WordMasterProject/src/com/mycom/word/Main.java
+++ WordMasterProject/src/com/mycom/word/Main.java
@@ -6,8 +6,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		//WordManger 에 있는 start 함수 호출하는 방법 
-		new WordManager().start(); 
-		
+		new WordManager().selectMenu();
+		//System.out.println("Hello world");
 	}
 
 }
